e690cb8df27588b38cf27a7da5d783f8c20bf121
jmcrego <jmcrego@gmail.com>
2020-09-28 16:15:39 +0200

    debug

--- CTranslate2.py
+++ CTranslate2.py
@@ -200,7 +200,8 @@ if __name__ == "__main__":
             logging.info('(translated batch {} with {} examples)'.format(b,len(batch)))
             for line in batch:
                 print('MySRC[{}]: {}'.format(n,' '.join(source[n])))
-                print('MyPRF[{}]: {}'.format(n,' '.join(prefix[n])))
+                if len(prefix[n]):
+                    print('MyPRF[{}]: {}'.format(n,' '.join(prefix[n])))
                 print('MyOUT[{}]: {}'.format(n,' '.join(line[0]['tokens'])))
                 if len(line):
                     hyp = " ".join(line[0]["tokens"])
